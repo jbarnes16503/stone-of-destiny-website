83fb28b516234b4fa90271a0063ede949abe9103
James Barnes <james.jbarnes@gmail.com>
2022-10-05 10:01:01 +0100

    Added code

---
+++ assets/php/form.php
@@ -0,0 +1,11 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$query = $_POST['query'];
+$formcontent=" From: $name \n Phone: $phone \n Email: $email \n Query: $query";
+$recipient = "contactform@stratagemstone.com";
+$subject = "Contact Form";
+$mailheader = "From: $recipient \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+?>
